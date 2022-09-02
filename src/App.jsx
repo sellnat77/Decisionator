176c92d5a38f99cdfecdb6d2a8ccec4f2eab3df2
Russell Tan <sell_nat@yahoo.com>
2022-09-01 19:39:33 -0700

    Fix yaml (#6)
    * Add branch
    
    * Dedent
    
    * indent env
    
    * Branches
    
    * Remove all consoles
    
    * use lib
    
    * use lib
    
    * use lib
    
    * Update deploy key
    
    * Update deploy key
    
    * Update deploy key
    
    * Update deploy key
    
    * Update deploy key
    
    * Try new deploy
    
    * Try new deploy
    
    * copy
    
    * homepage
    
    * homepage
    
    * homepage
    
    * homepage
    
    * homepage
    
    * homepage
    
    * Works, removing branch from push list
    
    * master -> main

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <CookiesProvider>
         <Header />
       </CookiesProvider>
