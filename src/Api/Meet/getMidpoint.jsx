a7b6e61dc4d706427764c506b4135625af443197
sellnat77 <sell_nat@yahoo.com>
2022-09-08 21:02:14 -0700

    Testing eureka

--- src/Api/Meet/getMidpoint.jsx
+++ src/Api/Meet/getMidpoint.jsx
@@ -4,9 +4,7 @@ const {
   REACT_APP_BACKEND_SERVER,
 } = process.env;
 async function getMidpoint(inputCoords) {
-  console.log(REACT_APP_BACKEND_SERVER);
   const res = await request.post(`${REACT_APP_BACKEND_SERVER}/meet/guest`, inputCoords);
-  console.log(res.body);
 
   return res.body;
 }
