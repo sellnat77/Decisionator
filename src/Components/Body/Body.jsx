87c11d0271328f45faabfa0b4aecc35258af6116
Russell Tan <sell_nat@yahoo.com>
2022-09-18 12:03:58 -0700

    Trying to output the lat/lon on place selected (#19)

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,23 +1,41 @@
 import './Body.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import AddressBox from './AddressInput/AddressBox';
 import MapThumbnail from './Map/MapThumbnail/MapThumbnail';
 import Map from './Map/Map';
 
 function Body() {
+  const [coords, setCoords] = useState([]);
+
   return (
     <div className="body">
       <Stack className="body__stack" direction="row" justifyContent="space-around" spacing={2}>
         <Stack className="body__stack__left" spacing={2}>
-          <AddressBox />
+          <AddressBox coords={coords} setCoords={setCoords} />
           <MapThumbnail />
         </Stack>
         <Stack className="body__stack__mid" spacing={2}>
           <Map />
+          <Button variant="outlined">
+            Logout
+          </Button>
+          {
+            coords.map((coord) => (
+              <div key={coord.id}>
+                <h3>
+                  { coord.lat }
+                </h3>
+                <h3>
+                  { coord.lon }
+                </h3>
+              </div>
+            ))
+          }
         </Stack>
         <Stack className="body__stack__right" spacing={2}>
-          <AddressBox />
+          <AddressBox coords={coords} setCoords={setCoords} />
           <MapThumbnail />
         </Stack>
       </Stack>
